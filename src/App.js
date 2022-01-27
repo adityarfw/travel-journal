650e2c6c47d6070b2e5b1edddc2a008e5cbebf6d
adityarfw <aditya.rajkumar@wisetail.com>
2022-01-27 14:41:32 -0500

    Added card and navbar component files, added more locations to data file, added images to public folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Card from './components/Card';
+import data from './data';
 
 const App = () => {
+  const cards = data.map((item) => {
+    return <Card key={item.title} {...item} />;
+  });
   return (
     <div>
       <Navbar />
+      {cards}
     </div>
   );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import globe from '../images/globe.png';
+// import globe from '../../public/';
 
 const Navbar = () => {
+  const globeImg = process.env.PUBLIC_URL + '/images/globe.png';
   return (
     <nav>
-      <img src={globe} className='nav-img' />
+      <img src={globeImg} className='nav-img' />
       <p>my travel journal.</p>
     </nav>
   );
